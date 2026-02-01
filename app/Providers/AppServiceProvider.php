6677db6c26d7a3ee4975d72a8c7798ad56f5e990
PierreCodex <cordovanoexiste@gmail.com>
2026-02-01 01:20:33 -0500

    fix: force https scheme in production for correct asset loading

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -28,5 +28,8 @@ class AppServiceProvider extends ServiceProvider
         //
         Schema::defaultStringLength(191);
 
+        if (config('app.env') === 'production') {
+            \Illuminate\Support\Facades\URL::forceScheme('https');
+        }
     }
 }
